9b677db47cf9f469335cff14034982b62dd5f94d
abidalili <abidalili1995@gmail.com>
2023-03-09 01:26:08 +0200

    created a clock by using BOM timing events

--- timing events(BOM)/index.js
+++ timing events(BOM)/index.js
@@ -19,10 +19,49 @@
 const saveButton = document.querySelector(".save-button");
 const mesasge = document.querySelector(".message");
 
-saveButton.addEventListener("click", saveUser);
+// saveButton.addEventListener("click", saveUser);
 
-function saveUser() {
-  mesasge.textContent = "user registration successful";
+// function saveUser() {
+//   mesasge.textContent = "user registration successful";
 
-  setTimeout(() => {}, 2000);
+//   setTimeout(() => {
+//     mesasge.textContent = "";
+//   }, 2000);
+// }
+
+// saveButton.addEventListener("click", startCount);
+// function startCount() {
+//   let count = 0;
+//   mesasge.textContent = count;
+
+//   setInterval(() => {
+//     count++;
+//     mesasge.textContent = count;
+//   }, 1000);
+// }
+
+saveButton.addEventListener("click", startClock);
+
+function startClock() {
+  let date = new Date();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+
+  hours = formatTime(hours);
+  minutes = formatTime(minutes);
+  seconds = formatTime(seconds);
+
+  let time = hours + ":" + minutes + ":" + seconds;
+
+  mesasge.textContent = time;
+
+  setInterval(startClock, 1000);
+}
+
+function formatTime(value) {
+  if (value < 10) {
+    value = "0" + value;
+  }
+  return value;
 }
